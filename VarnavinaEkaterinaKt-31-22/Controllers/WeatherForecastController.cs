4d9f63dfa1fb2f3b0a937d638624c70e724a40f0
Eva455989 <eva130504@gmail.com>
2025-02-21 19:51:47 +0300

    add loggin and WeatherForecast test recuest

--- VarnavinaEkaterinaKt-31-22/Controllers/WeatherForecastController.cs
+++ VarnavinaEkaterinaKt-31-22/Controllers/WeatherForecastController.cs
@@ -5,10 +5,17 @@ namespace VarnavinaEkaterinaKt_31_22.Controllers
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
+    {[HttpPost(Name ="AddNewSummary")]
+    public string[] AddNewSummary(string newSummary)
     {
-        private static readonly string[] Summaries = new[]
+        _logger.LogError("New method was called");
+        var list=Summaries.ToList();
+        list.Add(newSummary);
+        return list.ToArray();
+    }
+            private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Супер мега погода"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -21,6 +28,7 @@ namespace VarnavinaEkaterinaKt_31_22.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogError("Method was called");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
